402349711ecc94503ad8bca52ff39dd37dd6c630
Bert Klandermans <klandermans@users.noreply.github.com>
2021-06-04 14:54:10 +0200

    Update export.py

--- export.py
+++ export.py
@@ -1,6 +1,7 @@
 # Simple SQL database exporter
 
 # Dumps all the data & specific queries to disk and upload them to Azure Storage.
+# Can be used for the export of delpro software but also for other sql server software. I scheduled this software through the windows task scheduler to run every day. 
 
 # dependencies:
 # Windows or Linux SQL server
