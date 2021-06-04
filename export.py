7c5324ef05eac9388787f1f6ffc03d7557393e8e
Bert Klandermans <klandermans@users.noreply.github.com>
2021-06-04 14:50:09 +0200

    Update export.py

--- export.py
+++ export.py
@@ -1,4 +1,4 @@
-# Simple Deplro database exporter
+# Simple SQL database exporter
 
 # Dumps all the data & specific queries to disk and upload them to Azure Storage. Can be installed on the Deplro computer and schedulled with windows task scheduler or can be used on an restored backup. 
 
